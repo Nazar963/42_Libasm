2b7d67d693626f4f8c19bbf28ec96159e2d2c680
Nazar963 <nizarbonex1@gmail.com>
2023-11-19 17:55:45 +0100

    fin

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,38 +1,58 @@
 section .data
 	text1 db "Hello, World!",10,0
 	text2 db "What's your name?",10,0
-	format db "%ld",10,0
+	text3 db 0
+	text4 db "",0
+	error db "Hi, There's an error here!",10,0
+	format db "%ld",10,0 ;? Format for the printf call
 
 section .text
 	global main
-	extern printf
+	global ft_strlen
+	extern printf ;? To be able to use printf
 
 main:
-	lea rdi, [rel text1]
-	; mov rdi, text1
-	call _ft_strlen
+	mov rdi, text1
+	call ft_strlen
 	;* printf rax
 	mov rsi, rax
-	lea rdi, [rel format]
-	; mov rdi, format
+	mov rdi, format
 	xor rax, rax
-	call printf
+	call printf wrt ..plt
 
-	; mov rdi, text2
-	lea rdi, [rel text2]
-	call _ft_strlen
+	mov rdi, text2
+	call ft_strlen
 	;* printf rax
 	mov rsi, rax
-	; mov rdi, format
-	lea rdi, [rel format]
+	mov rdi, format
 	xor rax, rax
-	call printf
+	call printf wrt ..plt
+
+	mov rdi, text3
+	call ft_strlen
+	;* printf rax
+	mov rsi, rax
+	mov rdi, format
+	xor rax, rax
+	call printf wrt ..plt
+
+	mov rdi, text4
+	call ft_strlen
+	;* printf rax
+	mov rsi, rax
+	mov rdi, format
+	xor rax, rax
+	call printf wrt ..plt
 
 	mov rax, 60
 	xor rdi, rdi
 	syscall
 
-_ft_strlen:
+ft_strlen:
+	cmp rdi, 0
+	je _error_handle
+	cmp byte [rdi], 0
+	je _error_handle
 	xor rcx, rcx
 
 _ft_strlen_loop:
@@ -43,4 +63,14 @@ _ft_strlen_loop:
 
 _done:
 	mov rax, rcx
+	ret
+
+_error_handle:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, error
+	mov rdx, 28
+	syscall
+
+	xor rax, rax
 	ret
